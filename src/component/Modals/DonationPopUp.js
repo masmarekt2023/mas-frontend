0e720b95845357d6c3c95d3b3e56c7f3c67ac793
masmas2023 <130458447+masmas2023@users.noreply.github.com>
2023-06-06 09:42:35 +0300

    Update2 DonationPopUp.js

--- src/component/Modals/DonationPopUp.js
+++ src/component/Modals/DonationPopUp.js
@@ -691,7 +691,7 @@ export const DonationPopUp = ({ open, handleClose, userData }) => {
                     <Grid item xs={3} align="right">
                       <Typography variant="h5">transactionHash:</Typography>
                       <Typography variant="body2" component="label">
-                        {transactionHash}
+                        {serialNumber}
                       </Typography>
                     </Grid>
                   </Grid>
