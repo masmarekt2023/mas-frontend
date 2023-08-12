a97a6b91b974cb3786242fa65dc9973f664fff16
masmarekt2023 <127682794+masmarekt2023@users.noreply.github.com>
2023-08-12 16:15:00 +0300

    Update Apiconfigs.js

--- src/Apiconfig/Apiconfigs.js
+++ src/Apiconfig/Apiconfigs.js
@@ -3,10 +3,10 @@ export let baseURL = "https://node.masplatform.net";
 export let pageURL = "https://masplatform.net"
 export let socketURL = "wss://node.masplatform.net";
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+/*if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
    baseURL = "http://localhost:1865";
    socketURL = "ws://localhost:1865";
-}
+}*/
 
 const url = `${baseURL}/api/v1`;
 const Apiconfigs = {
