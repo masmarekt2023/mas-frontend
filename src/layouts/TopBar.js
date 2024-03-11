734f531d58625d7b08ed03bc2bc339f5e043e855
masmarekt2023 <127682794+masmarekt2023@users.noreply.github.com>
2024-03-11 05:56:58 +0300

    Update TopBar.js

--- src/layouts/TopBar.js
+++ src/layouts/TopBar.js
@@ -512,6 +512,27 @@ export default function Header() {
                       </Tooltip>
                     </Box>
                   )}
+                     {
+                  auth.userLoggedIn ? <>
+                  <Tooltip
+                      title="My Profile"
+                      placement="bottom"
+                    >
+                       <Avatar onClick={() => navigate("/profile")}
+                        alt={auth.userData?.userName} 
+                        src={auth.userData?.profilePic} 
+                        style={{cursor: 'pointer', border: 'solid 3px #fff'}} />
+                    </Tooltip>
+                    
+                    <Tooltip
+                      title="My Setting"
+                      placement="bottom"
+                    >
+                      <IconButton onClick={() => navigate('/profilesettings')}>
+                        <AiFillSetting />
+                      </IconButton>
+                    </Tooltip></> :
+}
                   <Box>
                     {auth.userLoggedIn ? (
 
